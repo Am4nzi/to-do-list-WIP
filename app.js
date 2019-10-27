101b0f5f3557358b9b06a2e5482bd35c87a54f14
Gareth <gvs.0027@gmail.com>
2019-10-27 17:59:52 +0100

    Start developing structure for layout

--- app.js
+++ app.js
@@ -7,57 +7,45 @@ app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
 
-//   if (currentDay === 0) {
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    var day = today.toLocaleDateString("en-US", options);
+
+
+
+//   var currentDay = today.getDay();
+//   var day = "";
+
+//   switch (currentDay) {
+//     case 0:
 //       day = "Sunday";
-//       res.render("list", {kindOfDay: day} )
-//   } else if (currentDay === 1) {
-//       day = "Monday"
-//       res.render("list", {kindOfDay: day} )
-//   } else if (currentDay === 2) {
-//       day = "Tuesday"
-//       res.render("list", {kindOfDay: day} )
-//   } else if (currentDay === 3) {
-//       day = "Wednesday"
-//       res.render("list", {kindOfDay: day} )
-//   } else if (currentDay === 4) {
-//       day = "Thursday"
-//       res.render("list", {kindOfDay: day} )
-//   } else if (currentDay === 5) {
-//       day = "Friday"
-//       res.render("list", {kindOfDay: day} )
-//   } else if (currentDay === 6) {
-//       day = "Saturday"
-//       res.render("list", {kindOfDay: day} )
+//       break;
+//     case 1:
+//       day = "Monday";
+//       break;
+//     case 2:
+//       day = "Tuesday";
+//       break;
+//     case 3:
+//       day = "Wednesday";
+//       break;
+//     case 4:
+//       day = "Thursday";
+//       break;
+//     case 5:
+//       day = "Friday";
+//       break;
+//     case 6:
+//       day = "Saturday";
+//       break;
+//       default:
+//         console.log("Error: current day is equal to: " + currentDay);
 //   }
-
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-      default:
-        console.log("Error: current day is equal to: " + currentDay);
-  }
 
   res.render("list", {
       kindOfDay: day
