b85808544a58f25bca130c9c8095058aed3488dc
Gareth <gvs.0027@gmail.com>
2019-10-29 17:53:14 +0100

    Add second list and add EJS partials for header and footer

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
@@ -13,28 +14,47 @@ app.set("view engine", "ejs");
 app.get("/", function(req, res) {
   let today = new Date();
 
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
 
-    let day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
   res.render("list", {
-      kindOfDay: day,
-      newListItems: items
+    listTitle: day,
+    newListItems: items
   });
-
 });
 
 app.post("/", function(req, res) {
+
   let item = req.body.newItem;
 
-  items.push(item);
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 
-  res.redirect("/");
+});
 
+app.get("/work", function(req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.get("/about", function(req, res) {
+  res.render("about");
+})
+
+app.post("work", function(req, res) {
+  let item = req.body.newItem;
+  workItems.push(item);
+  workItems.push(item);
+  res.redirect("/work");
 });
 
 app.listen(8080, function() {
