c2ee753f30aea589c0867b503edb4321fc6b4206
Gareth <gvs.0027@gmail.com>
2019-10-31 14:40:56 +0100

    complete stlying

--- app.js
+++ app.js
@@ -13,10 +13,6 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
-  res.render("home");
-})
-
-app.get("/today", function(req, res) {
 
   const day = date.getDate();
 
@@ -26,33 +22,14 @@ app.get("/today", function(req, res) {
   });
 });
 
-app.post("/today", function(req, res) {
+app.post("/", function(req, res) {
 
   const item = req.body.newItem;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
+
     items.push(item);
-    res.redirect("/today");
-  }
-
-});
-
-app.get("/thisweek", function(req, res) {
-  res.render("list", { listTitle: "Work List", newListItems: workItems });
-});
-
-app.get("/home", function(req, res) {
-  res.render("home");
-})
-
-app.post("thisweek", function(req, res) {
-  const item = req.body.newItem;
-  workItems.push(item);
-  workItems.push(item);
-  res.redirect("thisweek");
+    res.redirect("/");
+  
 });
 
 app.listen(8080, function() {
